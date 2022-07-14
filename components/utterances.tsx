8cad924880d2754fe1f1552a03bae2a009ff0eb0
chchaeun <kce3615@naver.com>
2022-07-14 16:17:12 +0900

    Feat: 댓글 추가

---
+++ components/utterances.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Utterances: React.FC = () => {
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return;
+        }
+        const scriptElem = document.createElement("script");
+        scriptElem.src = "https://utteranc.es/client.js";
+        scriptElem.async = true;
+        scriptElem.setAttribute("repo", "chchaeun/blog");
+        scriptElem.setAttribute("issue-term", "pathname");
+        scriptElem.setAttribute("theme", "github-light");
+        scriptElem.setAttribute("label", "blog-comment");
+        scriptElem.crossOrigin = "anonymous";
+        elem.appendChild(scriptElem);
+      }}
+    />
+  );
+};
+
+export default Utterances;
