eeddd206ed404ceb65b6798aeef93ff4e16cc17f
chchaeun <kce3615@naver.com>
2022-07-15 22:12:14 +0900

    Fix: 스파이 스크롤 중복 오류 수정

--- components/utterances.tsx
+++ components/utterances.tsx
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Utterances: React.FC = () => {
-  return (
-    <section
-      ref={(elem) => {
-        if (!elem) {
-          return;
-        }
+const Utterances = () => {
+  useEffect(() => {
+    const elem = document.querySelector("section");
+    const scriptElem = document.createElement("script");
+    scriptElem.src = "https://utteranc.es/client.js";
+    scriptElem.async = true;
+    scriptElem.setAttribute("repo", "chchaeun/blog");
+    scriptElem.setAttribute("issue-term", "pathname");
+    scriptElem.setAttribute("theme", "github-light");
+    scriptElem.setAttribute("label", "blog-comment");
+    scriptElem.crossOrigin = "anonymous";
 
-        const scriptElem = document.createElement("script");
-        scriptElem.src = "https://utteranc.es/client.js";
-        scriptElem.async = true;
-        scriptElem.setAttribute("repo", "chchaeun/blog");
-        scriptElem.setAttribute("issue-term", "pathname");
-        scriptElem.setAttribute("theme", "github-light");
-        scriptElem.setAttribute("label", "blog-comment");
-        scriptElem.crossOrigin = "anonymous";
-
-        const prevScript = document.querySelector("section > script");
-        if (!prevScript) {
-          elem.appendChild(scriptElem);
-        }
-      }}
-    />
-  );
+    const prevScript = document.querySelector("section > script");
+    if (!prevScript) {
+      elem?.appendChild(scriptElem);
+    }
+  }, []);
+  return <section />;
 };
 
 export default Utterances;
