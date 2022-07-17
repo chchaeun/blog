a83246cbde910dcc50004b1bb53b341b1ccf19fb
chchaeun <kce3615@naver.com>
2022-07-17 17:56:23 +0900

    Feat: Head favicon 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <title>개발새발</title>
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </Layout>
